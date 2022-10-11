9495d10bb55d8760e1aa22140551c874cdd443f7
Andrey I <iaa@bork.ru>
2022-10-11 09:21:01 +0200

    move parser logic to global

--- RabobankTA/Model/Service/CSVParser.swift
+++ RabobankTA/Model/Service/CSVParser.swift
@@ -8,16 +8,20 @@
 import Foundation
 
 class CSVParser: ParserProtocol {
-    func parseString(_ value: String, completion: (TableEntity)->()) {
-        var resultValue = [[String]]()
-        let rows = value.split(separator: "\r\n", omittingEmptySubsequences: true)
-        
-        for (index, row) in rows.enumerated() {
-            print(index, row)
-            let columns = row.split(separator: ",", omittingEmptySubsequences: true)
-            let columnsArray = columns.map{String($0)}
-            resultValue.append(columnsArray)
+    func parseString(_ value: String, completion: @escaping (TableEntity)->()) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            var resultValue = [[String]]()
+            let rows = value.split(separator: "\r\n", omittingEmptySubsequences: true)
+            
+            for (index, row) in rows.enumerated() {
+                print(index, row)
+                let columns = row.split(separator: ",", omittingEmptySubsequences: true)
+                let columnsArray = columns.map{String($0)}
+                resultValue.append(columnsArray)
+            }
+            DispatchQueue.main.async {
+                completion(TableEntity(rows: resultValue.map{RowEntity(values: $0)}))
+            }
         }
-        completion(TableEntity(rows: resultValue.map{RowEntity(values: $0)}))
     }
 }
